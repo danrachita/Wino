72a16662495e8c8adac6a2bee86fd8caba1626ad
danrachita <38796007+danrachita@users.noreply.github.com>
2019-01-19 22:33:50 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -109,7 +109,7 @@ public:
         nTargetTimespan = 1 * 60;
         nTargetSpacing = 1 * 60;
         nLastPOWBlock = 400;
-        nMaturity = 201;
+        nMaturity = 20;
         nMasternodeCountDrift = 20;
 	nMasternodeColleteralLimxDev = 5000; //Params().MasternodeColleteralLimxDev()
         nModifierUpdateBlock = 1; // we use the version 2 for dmd
